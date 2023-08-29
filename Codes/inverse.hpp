daf7f9ae73260edffe9dd8f15c3c5514a8d7ace1
Petro2806 <petro28062000@gmail.com>
2023-08-29 16:14:10 +0300

    Some refactoring

--- Codes/inverse.hpp
+++ Codes/inverse.hpp
@@ -12,18 +12,24 @@ VI inverse(VI a, int k)
 	int nk = (k + 1) / 2;
 	VI t = mult(a, ra);	
 	FOR(i, 0, nk)
+	{
 		if(2 * i < SZ(t))
+		{
 			t[i] = t[2 * i];
-
+		}
+	}
+	
 	t = inverse(t, nk);
 	
 	t.resize(k);
 	RFOR(i, nk, 1)
+	{
 		if(2 * i < SZ(t))
 		{
 			t[2 * i] = t[i];
 			t[i] = 0;
 		}
+	}
 		
 	return mult(ra, t);
 }
