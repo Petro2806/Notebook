4ed1b1d2039498c944add0b83e8eca7915f659fd
Petro2806 <petro28062000@gmail.com>
2023-09-12 21:25:06 +0300

    Add main algorithms with generating functions

--- Codes/inverse.hpp
+++ Codes/inverse.hpp
@@ -1,35 +1,32 @@
-VI inverse(VI a, int k)
+VI inverse(const VI& a, int k)
 {
-	a.resize(k);
+	assert(SZ(a) == k && a[0] != 0);
 	if(k == 1)
-		return {binpow(a[0], mod - 2)};	
+		return {binpow(a[0], mod - 2)};  
 	
 	VI ra = a;
 	FOR(i, 0, SZ(ra))
 		if(i & 1)
-			ra[i] = sub(0, ra[i]);		
+			ra[i] = sub(0, ra[i]);    
 	
 	int nk = (k + 1) / 2;
-	VI t = mult(a, ra);	
-	FOR(i, 0, nk)
-	{
-		if(2 * i < SZ(t))
-		{
-			t[i] = t[2 * i];
-		}
-	}
-	
-	t = inverse(t, nk);
-	
+	VI t = mult(a, ra);
 	t.resize(k);
+	
+	FOR(i, 0, nk)
+		t[i] = t[2 * i];
+  
+	t.resize(nk);
+	t = inverse(t, nk);
+	t.resize(k);
+  
 	RFOR(i, nk, 1)
 	{
-		if(2 * i < SZ(t))
-		{
-			t[2 * i] = t[i];
-			t[i] = 0;
-		}
+		t[2 * i] = t[i];
+		t[i] = 0;
 	}
-		
-	return mult(ra, t);
+	
+	VI res = mult(ra, t);
+	res.resize(k);
+	return res;
 }
