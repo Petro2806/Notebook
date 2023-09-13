a6603e061eada53a60ad8fdcd1b87c13b767dddf
Petro2806 <36917672+Petro2806@users.noreply.github.com>
2023-09-13 22:21:01 +0300

    Fix ALL

--- Codes/template.hpp
+++ Codes/template.hpp
@@ -4,7 +4,7 @@ using namespace std;
 #define FOR(i, a, b) for(int i = (a); i < (b); i++)
 #define RFOR(i, b, a) for(int i = (b) - 1; i >= (a); i--)
 #define SZ(a) (int)a.size()
-#define ALL(a) a.begin(), e.end()
+#define ALL(a) a.begin(), a.end()
 #define PB push_back
 #define MP make_pair
 #define F first
