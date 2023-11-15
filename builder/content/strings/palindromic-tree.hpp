7e14ce9b9d8cb2f032f275ea90e3d3757654b99d
Petro2806 <petro28062000@gmail.com>
2023-11-15 20:46:50 +0200

    Fix palindromic tree

--- builder/content/strings/palindromic-tree.hpp
+++ builder/content/strings/palindromic-tree.hpp
@@ -13,52 +13,54 @@ struct Node
 struct PalTree
 {
 	string s;
-	vector<Node> A;
+	vector<Node> a;
 	int sz;
 	int last;
 	
 	void init(string t)
 	{
-		A.resize(2 * SZ(t));
-		A[0].clear();
-		A[1].clear();
-		A[1].len = 0;
-		A[1].link = 0;
+		a.resize(2 * SZ(t));
+		a[0].clear();
+		a[1].clear();
+		a[1].len = 0;
+		a[1].link = 0;
 		sz = 2;
 		last = 1;
 		s = t;
 	}
 	void add(int idx)
 	{
+		int ch = s[idx] - 'a';
+		
 		int cur = last;
 		while (cur != -1)
 		{
-			int pos = idx - A[cur].len - 1;
+			int pos = idx - a[cur].len - 1;
 			if (pos >= 0 && s[pos] == s[idx])
 				break;
-			cur = A[cur].link;
+			cur = a[cur].link;
 		}
 		assert(cur != -1);
-		if (A[cur].to[s[idx] - 'a'] == -1)
+		if (a[cur].to[ch] == -1)
 		{
-			A[cur].to[s[idx] - 'a'] = sz;
-			A[sz].clear();
-			A[sz].len = A[cur].len + 2;
-			int link = A[cur].link;
+			a[cur].to[ch] = sz;
+			a[sz].clear();
+			a[sz].len = a[cur].len + 2;
+			int link = a[cur].link;
 			while (link != -1)
 			{
-				int pos = idx - A[link].len - 1;
+				int pos = idx - a[link].len - 1;
 				if (pos >= 0 && s[pos] == s[idx])
 					break;
-				link = A[link].link;
+				link = a[link].link;
 			}
 			if (link == -1)
 				link = 1;
 			else
-				link = A[link].to[s[idx] - 'a'];
-			A[sz].link = link;
+				link = a[link].to[ch];
+			a[sz].link = link;
 			sz++;
 		}
-		last = A[cur].to[s[idx] - 'a'];
+		last = a[cur].to[ch];
 	}
-} pt;
+};
