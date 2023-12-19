6a29a0e1965e36ab793b162b5a3a48a883af8f9f
mshcherba-LeetCode <mshcherba2000@gmail.com>
2023-12-19 17:14:03 +0200

    Add algorithm for building a dual graph to a planar graph

---
+++ builder/content/geometry/point.hpp
@@ -0,0 +1,112 @@
+struct Pt
+{
+	db x, y;
+	Pt operator+(const Pt& p) const
+	{
+		return {x + p.x, y + p.y};
+	}
+	Pt operator-(const Pt& p) const
+	{
+		return {x - p.x, y - p.y};
+	}
+	Pt operator*(db d) const
+	{
+		return {x * d, y * d};
+	}
+	Pt operator/(db d) const
+	{
+		return {x / d, y / d};
+	}
+};
+// Returns the squared absolute value
+db sq(const Pt& p)
+{
+	return p.x * p.x + p.y * p.y;
+}
+// Returns the absolute value
+db abs(const Pt& p)
+{
+	return sqrt(sq(p));
+}
+// Returns -1 for negative numbers, 0 for zero,
+// and 1 for positive numbers
+int sgn(db x)
+{
+	return (EPS < x) - (x < -EPS);
+}
+// Returns `p` rotated counter-clockwise by `a`
+Pt rot(const Pt& p, db a)
+{
+	db co = cos(a), si = sin(a);
+	return {p.x * co - p.y * si,
+		p.x * si + p.y * co};
+}
+// Returns `p` rotated counter-clockwise by 90
+Pt perp(const Pt& p)
+{
+	return {-p.y, p.x};
+}
+// Returns the dot product of `p` and `q`
+db dot(const Pt& p, const Pt& q)
+{
+	return p.x * q.x + p.y * q.y;
+}
+// Returns the angle between `p` and `q`
+db angle(const Pt& p, const Pt& q)
+{
+	return acos(clamp(dot(p, q) / abs(p) /
+		abs(q), (db)-1.0, (db)1.0));
+}
+// Returns the cross product of `p` and `q`
+db cross(const Pt& p, const Pt& q)
+{
+	return p.x * q.y - p.y * q.x;
+}
+// Positive if R is on the left side of PQ,
+// negative on the right side,
+// and zero if R is on the line containing PQ
+db orient(const Pt& p, const Pt& q, const Pt& r)
+{
+	return cross(q - p, r - p) / abs(q - p);
+}
+// Checks if a polygon `v` is convex
+bool isConvex(const vector<Pt>& v)
+{
+	bool hasPos = false, hasNeg = false;
+	int n = SZ(v);
+	FOR(i, 0, n)
+	{
+		int o = sgn(orient(v[i], v[(i + 1) % n],
+			v[(i + 2) % n]));
+		hasPos |= o > 0;
+		hasNeg |= o < 0;
+	}
+	return !(hasPos && hasNeg);
+}
+// Checks if argument of `p` is in [-pi, 0)
+bool half(const Pt& p)
+{
+	assert(sgn(p.x) != 0 || sgn(p.y) != 0);
+	return sgn(p.y) == -1 ||
+		(sgn(p.y) == 0 && sgn(p.x) == -1);
+}
+// Polar sort of vectors in `v` around `o`
+void polarSortAround(const Pt& o, vector<Pt>& v)
+{
+	sort(ALL(v), [o](const Pt& p, const Pt& q)
+	{
+		bool hp = half(p - o), hq = half(q - o);
+		if (hp != hq)
+			return hp < hq;
+		int s = sgn(cross(p, q));
+		if (s != 0)
+			return s == 1;
+		return sq(p - o) < sq(q - o);
+	});
+}
+// Example:
+// cout << a + b << " " << a - b << "\n";
+ostream& operator<<(ostream& os, const Pt& p)
+{
+	return os << "(" << p.x << "," << p.y << ")";
+}
