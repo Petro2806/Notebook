eaf0c272d084e380bfe7bc2cb1ed11bf6993db5e
mshcherba-LeetCode <mshcherba2000@gmail.com>
2024-02-10 16:45:16 +0200

    divide into separate functions

--- builder/content/graphs/hierholzer.hpp
+++ builder/content/graphs/hierholzer.hpp
@@ -1,60 +1,66 @@
 /**
- * Description: Finds an Eulerian path in a directed or undirected (defined by template parameter) graph.
- * Most likely, you need only one directedness and can delete some code.
+ * Description: Finds an Eulerian path in a directed or undirected graph.
  * $g$ is a graph with $n$ vertices. $g[u]$ is a vector of pairs $(v, \text{edge_id})$. $m$ is the number of edges in the graph.
  * The vertices are numbered from $0$ to $n - 1$, and the edges - from $0$ to $m - 1$.
  * If there is no Eulerian path, returns {{-1}, {-1}}.
  * Otherwise, returns the path in the form (vertices, edges) with vertices containing $m + 1$ elements and edges containing $m$ elements.
- * The edge with number edges$[i]$ is from vertices$[i]$ to vertices$[i + 1]$.
  * If you need an Eulerian cycle, check vertices[0] = vertices.back().
  */
-template<bool directed>
-pair<VI, VI> hierholzer(vector<vector<PII>> g, int m)
+// 528807 for undirected
+tuple<bool, int, int> checkDirected(vector<vector<PII>>& g)
 {
 	int n = SZ(g), v1 = -1, v2 = -1;
 	bool bad = false;
-	if (directed)
+	VI degIn(n);
+	FOR(u, 0, n)
+		for (auto [v, e] : g[u])
+			degIn[v]++;
+	FOR(u, 0, n)
 	{
-		VI degIn(n);
-		FOR(u, 0, n)
-			for (auto [v, e] : g[u])
-				degIn[v]++;
-		FOR(u, 0, n)
+		bad |= abs(degIn[u] - SZ(g[u])) > 1;
+		if (degIn[u] < SZ(g[u]))
 		{
-			bad |= abs(degIn[u] - SZ(g[u])) > 1;
-			if (degIn[u] < SZ(g[u]))
-			{
-				bad |= v2 != -1;
-				v2 = u;
-			}
-			else if (degIn[u] > SZ(g[u]))
-			{
-				bad |= v1 != -1;
+			bad |= v2 != -1;
+			v2 = u;
+		}
+		else if (degIn[u] > SZ(g[u]))
+		{
+			bad |= v1 != -1;
+			v1 = u;
+		}
+	}
+	return {bad, v1, v2};
+}
+
+/*tuple<bool, int, int> checkUndirected(vector<vector<PII>>& g)
+{
+	int n = SZ(g), v1 = -1, v2 = -1;
+	bool bad = false;
+	FOR(u, 0, n)
+	{
+		if (SZ(g[u]) & 1)
+		{
+			bad |= v2 != -1;
+			if (v1 == -1)
 				v1 = u;
-			}
+			else
+				v2 = u;
 		}
 	}
-	else
-	{
-		FOR(u, 0, n)
-		{
-			if (SZ(g[u]) & 1)
-			{
-				bad |= v2 != -1;
-				if (v1 == -1)
-					v1 = u;
-				else
-					v2 = u;
-			}
-		}
-	}
+	return {bad, v1, v2};
+}*/
+
+pair<VI, VI> hierholzer(vector<vector<PII>> g, int m)
+{
+	// checkUndirected if undirected
+	auto [bad, v1, v2] = checkDirected(g);
 	if (bad)
 		return {{-1}, {-1}};
 	if (v1 != -1)
 	{
 		g[v1].PB({v2, m});
-		if (!directed)
-			g[v2].PB({v1, m});
+		// uncomment if undirected
+		//g[v2].PB({v1, m});
 		m++;
 	}
 	deque<PII> d;
