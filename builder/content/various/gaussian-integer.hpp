05adb971cee2a7206c950d71b781e68d9d5300f8
Petro2806 <petro28062000@gmail.com>
2024-02-10 21:09:35 +0200

    Added gaussian integers

--- builder/content/various/gaussian-integer.hpp
+++ builder/content/various/gaussian-integer.hpp
@@ -1,12 +1,13 @@
 /**
- * Description: Works for int.
- *
+ * Description: $n = am + b, \frac{n}{m} = a, n \% m = b$.
+ * use \_\_gcd instead of gcd.\\
+ * If $g = gcd(\dots)$ than $\frac{a_i}{g}$ is the new coordinates with respect to $g$.
  */
 LL closest(LL u, LL d)
 {
-	if(2 * u + d >= 0)
+	if(u >= 0)
 		return (2 * u + d) / (2 * d);
-	return -((-2 * u + d) / (2 * d));	
+	return -closest(-u, d);
 }
 struct num : complex<LL>
 {	
@@ -16,7 +17,7 @@ struct num : complex<LL>
 	{
 		return num(real(), -imag());
 	}
-	num operator% (num x)
+	num operator/ (num x)
 	{
 		num prod = *this * x.inv();
 		LL D = (x * x.inv()).real();
@@ -24,17 +25,24 @@ struct num : complex<LL>
 		LL m = closest(prod.real(), D);
 		LL n = closest(prod.imag(), D);
 	
-		return *this - x * num(m, n);
+		return num(m, n);
 	}
-	bool operator != (num b)
+	num operator% (num x)
+	{
+		return *this - x * (*this / x);
+	}
+	bool operator == (num b)
 	{
 		FOR(it, 0, 4)
 		{
 			if(real() == b.real() && imag() == b.imag())
-				return false;
+				return true;
 			b = b * num(0, 1);
 		}
-		return true;
+		return false;
+	}
+	bool operator != (num b)
+	{
+		return !(*this == b);
 	}
 };
-
