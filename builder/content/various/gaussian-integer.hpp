84f5afacdd5d6cae776079b1e08a49afd1007d3d
Petro2806 <petro28062000@gmail.com>
2024-02-28 12:22:22 +0200

    Fixes

--- builder/content/various/gaussian-integer.hpp
+++ builder/content/various/gaussian-integer.hpp
@@ -1,26 +1,25 @@
 /**
  * Description: $n = am + b, \frac{n}{m} = a, n \% m = b$.
  * use \_\_gcd instead of gcd.\\
- * If $g = gcd(\dots)$ than $\frac{a_i}{g}$ is the new coordinates with respect to $g$.
+ * Facts: Primes of the form $4n + 3$ are Gaussian primes.
+ * Uniqueness of prime factorization.
  */
 LL closest(LL u, LL d)
 {
-	if(u >= 0)
-		return (2 * u + d) / (2 * d);
-	return -closest(-u, d);
+	if(d < 0)
+		return closest(-u, -d);
+	if(u < 0)
+		return -closest(-u, d);
+	return (2 * u + d) / (2 * d);
 }
 struct num : complex<LL>
 {	
 	num(LL a, LL b = 0) : complex(a, b) {}
 	num(complex a) : complex(a) {}
-	num inv()
-	{
-		return num(real(), -imag());
-	}
 	num operator/ (num x)
 	{
-		num prod = *this * x.inv();
-		LL D = (x * x.inv()).real();
+		num prod = *this * conj(x);
+		LL D = (x * conj(x)).real();
 		
 		LL m = closest(prod.real(), D);
 		LL n = closest(prod.imag(), D);
