9d69757c32fbd19cc8f7c2a7bb88122d37f96709
mshcherba-LeetCode <mshcherba2000@gmail.com>
2024-05-27 13:33:36 +0300

    add an algorithm for the shortest path problem for graphs with Monge edge weights

---
+++ builder/content/graphs/monge-shortest-path.hpp
@@ -0,0 +1,40 @@
+/**
+ * Description: Finds shortest paths from the vertex $0$ to all vertices in a DAG with $n$ vertices,
+ * where the edges weights $c(i, j)$ satisfy the Monge property:
+ * $\forall i, j, k, l, \quad 0 \le i < j < k < l < n \implies c(i, l) + c(j, k) \ge c(i, k) + c(j, l)$.
+ * Time: O(n \log n)
+ */
+
+template<typename F>
+vector<LL> mongeShortestPath(int n, const F& cost)
+{
+	vector<LL> dp(n, LINF);
+	VI amin(n);
+	dp[0] = 0;
+	
+	auto update = [&](int i, int k)
+	{
+		LL nd = dp[k] + cost(k, i);
+		if (nd < dp[i])
+		{
+			dp[i] = nd;
+			amin[i] = k;
+		}
+	};
+	
+	function<void(int, int)> solve = [&](int l, int r)
+	{
+		if (r - l == 1)
+			return;
+		int m = (l + r) / 2;
+		FOR(k, amin[l], min(m, amin[r] + 1))
+			update(m, k);
+		solve(l, m);
+		FOR(k, l + 1, m + 1)
+			update(r, k);
+		solve(m, r);
+	};
+	
+	solve(0, n - 1);
+	return dp;
+}
