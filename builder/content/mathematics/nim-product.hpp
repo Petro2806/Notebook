c927351577253eee7e4f1bc94251127377fa30f2
mshcherba-LeetCode <mshcherba2000@gmail.com>
2024-07-09 12:22:31 +0300

    minor fixes

--- builder/content/mathematics/nim-product.hpp
+++ builder/content/mathematics/nim-product.hpp
@@ -8,27 +8,29 @@ typedef unsigned long long ULL;
 
 const int S = 8;
 
-ULL small[1 << S][1 << S];
-
-template <bool isPre = false>
-ULL nimProduct(ULL a, ULL b, int p = 64)
-{
-	if (min(a, b) <= 1)
-		return a * b;
-	if (!isPre && p <= S)
-		return small[a][b];
-	p >>= 1;
-	ULL a1 = a >> p, a2 = a & ((1ULL << p) - 1);
-	ULL b1 = b >> p, b2 = b & ((1ULL << p) - 1);
-	ULL c = nimProduct<isPre>(a1, b1, p);
-	ULL d = nimProduct<isPre>(a2, b2, p);
-	ULL e = nimProduct<isPre>(a1 ^ a2, b1 ^ b2, p);
-	return nimProduct<isPre>(c, 1ULL << (p - 1), p) ^ d ^ ((d ^ e) << p);
-}
+int small[1 << S][1 << S];
 
 void init()
 {
 	FOR(i, 0, 1 << S)
 		FOR(j, 0, 1 << S)
-			small[i][j] = nimProduct<true>(i, j, S);
+			small[i][j] = -1;
+}
+
+ULL nimProduct(ULL a, ULL b, int p = 64)
+{
+	if (min(a, b) <= 1)
+		return a * b;
+	if (p <= S && small[a][b] != -1)
+		return small[a][b];
+	p >>= 1;
+	ULL a1 = a >> p, a2 = a & ((1ULL << p) - 1);
+	ULL b1 = b >> p, b2 = b & ((1ULL << p) - 1);
+	ULL c = nimProduct(a1, b1, p);
+	ULL d = nimProduct(a2, b2, p);
+	ULL e = nimProduct(a1 ^ a2, b1 ^ b2, p);
+	ULL res = nimProduct(c, 1ULL << (p - 1), p) ^ d ^ ((d ^ e) << p);
+	if (p <= S / 2)
+		small[a][b] = res;
+	return res;
 }
