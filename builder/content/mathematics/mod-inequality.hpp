f7e2d1defa5fd9819d7e2710a2ef7cb52ca304f8
mshcherba-LeetCode <mshcherba2000@gmail.com>
2024-07-12 14:51:39 +0300

    rename the function

--- builder/content/mathematics/mod-inequality.hpp
+++ builder/content/mathematics/mod-inequality.hpp
@@ -2,7 +2,7 @@
  * Description: Finds the smallest $x \ge 0$ such that $(ax + b) \mod m \ge c$. Returns $-1$, if the solution does not exist.
  * Time: O(\log m).
  */
-int solveModInequality(LL m, LL a, LL b, LL c)
+int modInequality(LL m, LL a, LL b, LL c)
 {
 	a %= m;
 	b %= m;
@@ -12,7 +12,7 @@ int solveModInequality(LL m, LL a, LL b, LL c)
 		return -1;
 	if (c + a < m)
 		return (c - b + a - 1) / a;
-	int k = solveModularInequality(a, m, c - b - 1, c + a - m);
+	int k = modInequality(a, m, c - b - 1, c + a - m);
 	if (k == -1)
 		return -1;
 	return (k * m + c - b + a - 1) / a;
