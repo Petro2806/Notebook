28e2d7dbf88599a427183626f0dd6e4a35f2eec7
mshcherba-LeetCode <mshcherba2000@gmail.com>
2024-08-16 16:32:51 +0300

    add Bellman-Ford-Moore algorithm

---
+++ builder/content/graphs/bellman-ford-moore.hpp
@@ -0,0 +1,39 @@
+/**
+ * Description: Computes shortest paths from a single source vertex to all of the other vertices in a weighted directed graph.
+ * Time: O(n m)
+ */
+VL spfa(int n, int s)
+{
+	VL dist(n, LINF);
+	dist[s] = 0;
+	queue<int> q;
+	q.push(s);
+	VI inQueue(n);
+	inQueue[s] = true;
+	VI cnt(n);
+	bool negCycle = false;
+	while (!q.empty())
+	{
+		int v = q.front();
+		q.pop();
+		cnt[v]++;
+		negCycle |= cnt[v] > n;
+		inQueue[v] = false;
+		for (auto [to, w] : g[v])
+		{
+			LL newDist = dist[v] + w;
+			if (newDist < dist[to])
+			{
+				dist[to] = newDist;
+				if (!inQueue[to])
+				{
+					q.push(to);
+					inQueue[to] = true;
+				}
+			}
+		}
+		if (negCycle)
+			break;
+	}
+	return dist;
+}
